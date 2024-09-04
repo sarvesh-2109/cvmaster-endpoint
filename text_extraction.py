f47332c6adb6695973c5a51b199c766ff57c784d
sarvesh-2109 <sarveshmhatre213@gmail.com>
2024-09-05 01:17:04 +0530

    Updated feedback prompt and removed the sectioning from text_extraction

--- text_extraction.py
+++ text_extraction.py
@@ -9,6 +9,7 @@ async def get_pdf_text(file_stream):
     text = ""
     for page in doc:
         text += page.get_text()
+
     return text
 
 
@@ -18,6 +19,7 @@ async def get_docx_text(file_stream):
     text = ""
     for para in doc.paragraphs:
         text += para.text + "\n"
+
     return text
 
 
@@ -39,29 +41,5 @@ def remove_special_characters(text):
 def preprocess_text(text):
     """Preprocess text by removing special characters and organizing it into sections."""
     text = remove_special_characters(text)
-    lines = text.split('\n')
-    header, experience, cgpa, skills, projects, extracurricular = [], [], [], [], [], []
-    current_section = "header"
-    for line in lines:
-        if re.match(r'(EXPERIENCE|Experience|experience)', line):
-            current_section = "experience"
-        elif re.match(r'(SKILLS|Skills|skills)', line):
-            current_section = "skills"
-        elif re.match(r'(PROJECTS|Projects|projects)', line):
-            current_section = "projects"
-        elif re.match(r'(EXTRACURRICULAR|Extracurricular|extracurricular)', line):
-            current_section = "extracurricular"
-        else:
-            if current_section == "experience":
-                experience.append(line)
-            elif current_section == "skills":
-                skills.append(line)
-            elif current_section == "projects":
-                projects.append(line)
-            elif current_section == "extracurricular":
-                extracurricular.append(line)
-            else:
-                header.append(line)
-    result = "\n".join(header) + "\n\n" + "\n".join(experience) + "\n\n" +"\n\n" + "\n".join(skills) + \
-             "\n\n" + "\n".join(projects) + "\n\n" + "\n".join(extracurricular) + "\n\n"
-    return result.strip()
+
+    return text.strip()
