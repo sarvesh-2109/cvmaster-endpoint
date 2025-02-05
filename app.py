c1ea4e05d62617067f51dbd49f51bde785f0b93f
sarvesh-2109 <sarveshmhatre213@gmail.com>
2025-02-06 01:10:10 +0530

    Added loding before every page

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ class User(UserMixin, db.Model):
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 
 # Define the Resume model
